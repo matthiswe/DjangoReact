35a6ae5bd4fcd88920870d8a52fac88c1168120b
matthiswe <matthis.westermann@gamil.com>
2021-03-19 11:52:01 +0100

    19.03

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelpertText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
+import { FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel, TextField, Button, Grid, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 export default class CreateRoomPage extends Component{
@@ -60,9 +52,9 @@ export default class CreateRoomPage extends Component{
             </Grid>
             <Grid item xs={12} align="center">
                 <FormControl component="fieldset">
-                    <FormHelpertText component={'div'}>
+                    <FormHelperText component={'div'}>
                         <div align="center">Guest Control Playback State</div>
-                    </FormHelpertText>
+                    </FormHelperText>
                         <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
                             <FormControlLabel value="true" control={<Radio color="primary" />} label="Play/Pause" labelPlacement="bottom"/>
                             <FormControlLabel value="false" control={<Radio color="secondary" />} label="No Control" labelPlacement="bottom"/>
@@ -72,9 +64,9 @@ export default class CreateRoomPage extends Component{
             <Grid item xs={12} align="center">
                 <FormControl>
                     <TextField required={true} type="number" defaultvotes={this.defaultvotes} onChange={this.handleVotesChange} inputProps={{min:1, style:{textAlign:"center"}}}/>
-                        <FormHelpertText component={'div'}>
+                        <FormHelperText component={'div'}>
                             <div align="center">Votes required to skip a song</div>
-                        </FormHelpertText>
+                        </FormHelperText>
                 </FormControl>
             </Grid>
             <Grid item xs={12} align="center">
